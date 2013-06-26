c32e8d53b8f76c2a397ffcdc97992fd07cc85134
cluther <cluther@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2013-06-26 16:05:09 +0000

    Remove Zenoss from INSTALL_REQUIRES.
    This is to allow trunk ZenPacks to be tested on current stable Zenoss
    versions.
    
    Reviewed by gstreiff and jcausey.
    
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.DnsMonitor@74543 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ AUTHOR = 'Zenoss'
 LICENSE = ''
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
 PACKAGES = ['ZenPacks', 'ZenPacks.zenoss', 'ZenPacks.zenoss.DnsMonitor']
-INSTALL_REQUIRES = ['Zenoss >= 2.5.70']
+INSTALL_REQUIRES = []
 COMPAT_ZENOSS_VERS = '>=2.5.70'
 PREV_ZENPACK_NAME = 'DnsMonitor'
 # STOP_REPLACEMENTS
