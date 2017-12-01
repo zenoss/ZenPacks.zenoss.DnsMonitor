2c2225786cd5a2261a1e38e76c82f002ca74e372
romanbakaleyko <roman.bakaleyko@gmail.com>
2017-12-01 18:43:25 +0200

    ZEN-29196 Zenhub traceback due to empty cycle time in datasource.

--- ZenPacks/zenoss/DnsMonitor/interfaces.py
+++ ZenPacks/zenoss/DnsMonitor/interfaces.py
@@ -16,6 +16,6 @@ from Products.Zuul.utils import ZuulMessageFactory as _t
 class IDnsMonitorDataSourceInfo(IRRDDataSourceInfo):
     timeout = schema.Int(title=_t(u'Timeout (seconds)'))
     hostname = schema.TextLine(title=_t(u'Host Name'))
-    cycletime = schema.Int(title=_t(u'Cycle Time (seconds)'))
+    cycletime = schema.TextLine(title=_t(u'Cycle Time (seconds)'))
     dnsServer = schema.TextLine(title=_t(u'DNS Server'))
     expectedIpAddress = schema.TextLine(title=_t(u'Expected IP Address'))
